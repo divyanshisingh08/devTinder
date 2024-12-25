97103104ebcd794fba3a45d0526cb287d9f0c9b4
divyanshisingh08 <divyanshisingh2308@gmail.com>
2024-12-25 13:22:53 +0530

    route handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,39 @@ const express=require("express")
 
 const app= express();
 
+
+
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Divyanshi"} )
+})
+app.post("/user",(req,res)=>{
+    res.send("Data Saved Successfullu")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("User Deleted ")
+})
+app.get("/a(bc)?d",(req,res)=>{
+    res.send("a(bc)?d" )
+})
+
+app.get("/ab*c",(req,res)=>{
+    res.send("abbbbbbbbbbc" )
+})
+
+
+app.get(/.*fly$/,(req,res)=>{
+    res.send("Ends with fly" )
+})
+
+app.get("/user/:userId/:password",(req,res)=>{
+    console.log(req.params)
+    res.send("query params")
+})
+
+
+
 app.listen(2000,()=>{
     console.log("Server listening on 2000")
 })
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from 2000 test")
-})
-app.use("/watch",(req,res)=>{
-    res.send("Hello from 2000 test")
-})
-app.use((req,res)=>{
-    res.send("Hello from 2000")
-})
