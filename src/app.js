f042e8036afef4b30af8460af21dffedd01e5f72
divyanshisingh08 <divyanshisingh2308@gmail.com>
2024-12-27 23:53:28 +0530

    Error-handling

--- src/app.js
+++ src/app.js
@@ -1,145 +1,112 @@
-const express=require("express")
+const express = require("express");
 
-const app= express();
-const {adminAuth}= require("./middlewares/auth")
+const app = express();
+const { adminAuth } = require("./middlewares/auth");
 
-//ROUTES
+// ROUTES
 
-// app.get("/user",(req,res)=>{
-//     res.send({firstName: "Divyanshi"} )
-// })
-// app.post("/user",(req,res)=>{
-//     res.send("Data Saved Successfullu")
-// })
+// Example User Routes
+// app.get("/user", (req, res) => {
+//     res.send({ firstName: "User" });
+// });
 
-// app.delete("/user",(req,res)=>{
-//     res.send("User Deleted ")
-// })
-// app.get("/a(bc)?d",(req,res)=>{
-//     res.send("a(bc)?d" )
-// })
+// app.post("/user", (req, res) => {
+//     res.send("Data Saved Successfully");
+// });
 
-// app.get("/ab*c",(req,res)=>{
-//     res.send("abbbbbbbbbbc" )
-// })
+// app.delete("/user", (req, res) => {
+//     res.send("User Deleted");
+// });
 
+// Route Matching Examples
+// app.get("/a(bc)?d", (req, res) => {
+//     res.send("Matched pattern: a(bc)?d");
+// });
 
-// app.get(/.*fly$/,(req,res)=>{
-//     res.send("Ends with fly" )
-// })
+// app.get("/ab*c", (req, res) => {
+//     res.send("Matched pattern: ab*c");
+// });
 
-// app.get("/user/:userId/:password",(req,res)=>{
-//     console.log(req.params)
-//     res.send("query params")
-// })
+// app.get(/.*fly$/, (req, res) => {
+//     res.send("Matched pattern ending with 'fly'");
+// });
 
+// Route Parameters Example
+// app.get("/user/:userId/:password", (req, res) => {
+//     console.log(req.params);
+//     res.send("Received query parameters");
+// });
 
-//ROUTE HANDLERS & MIDDLEWARES
+// ROUTE HANDLERS & MIDDLEWARES
 
 // app.use(
 //     "/user",
-
-//     (req,res,next)=>{
-//         console.log("Handling Route handler 1")
-//         // res.send("Response 1"),
-//     next()
+//     (req, res, next) => {
+//         console.log("Executing Route Handler 1");
+//         next();
 //     },
-
-    
-//  [ (req,res,next)=>{
-//         console.log("Handling Route handler 2")
-//         // res.send("Response 2"),
-//         next()
-//     },
-
-//     (req,res,next)=>{
-//         console.log("Handling Route handler 3")
-//         res.send("Response 3"),
-//         next()
-//     }],
-    
-//     (req,res,next)=>{
-//         console.log("Handling Route handler 4")
-//         // next()
-      
+//     [
+//         (req, res, next) => {
+//             console.log("Executing Route Handler 2");
+//             next();
+//         },
+//         (req, res, next) => {
+//             console.log("Executing Route Handler 3");
+//             res.send("Response from Handler 3");
+//             next();
+//         }
+//     ],
+//     (req, res, next) => {
+//         console.log("Executing Route Handler 4");
 //     }
-//    )
+// );
 
+// USE OF MIDDLEWARES
 
-//USE OF MIDDLEWARES
-//  app.get(
-//     "/admin/getAllData",
-//     (req,res,next)=>{
-        
-//         //Logic to check if request is authorized
-//         const token="xyz";
-//         const isAuthorized =token=="zyz"
-//         if(isAuthorized){
-//             res.send("All Data Sent")
-//         }
-//         else {
-//             res.status(401).send("Unauthorized Request")
-//         }
+// app.get("/admin/getAllData", (req, res, next) => {
+//     const token = "xyz";
+//     const isAuthorized = token === "zyz";
+//     if (isAuthorized) {
+//         res.send("All Data Sent");
+//     } else {
+//         res.status(401).send("Unauthorized Request");
+//     }
+// });
 
-//  })
-
-
-//  app.get(
-//     "/admin/deleteUser",
-//     (req,res,next)=>{
-
-//         //Logic to check if request is authorized
-//         const token="xyz";
-//         const isAuthorized =token=="zyz"
-//         if(isAuthorized){
-//             res.send("User Deleted")
-//         }
-//         else {
-//             res.status(401).send("Unauthorized Request")
-//         }
-
-//  })
-
-
-
+// app.get("/admin/deleteUser", (req, res, next) => {
+//     const token = "xyz";
+//     const isAuthorized = token === "zyz";
+//     if (isAuthorized) {
+//         res.send("User Deleted");
+//     } else {
+//         res.status(401).send("Unauthorized Request");
+//     }
+// });
 
 /**
- * We are implementing logic of admin again and again that is where middleware comes into picture. 
- * 
- * 
- * Generally we use - app.use to write middleware so that all the request coming to a particular routes is handled by it first then will move to next handler ,
- * 
- * 
- * what we can do is - first we will write the Auth logic then if user is not authorized then throw error from there itseld and if admin is authorized then
- * move to using next()
+ * Middleware Optimization: Repetitive logic for authorization can be avoided by using a middleware function.
  */
 
+// app.use("/admin", adminAuth);
+
+// app.get("/admin/getAllData", (req, res) => {
+//     res.send("All Data Sent");
+// });
+
+// app.delete("/admin/deleteUser", (req, res) => {
+//     res.send("User Deleted");
+// });
+
+// ERROR HANDLING MIDDLEWARE
+// Note: The order of middleware matters. Error-handling middleware should be defined last.
+app.use("/",(err, req, res, next) => {
+    if (err) {
+        res.status(500).send("An error occurred on the server");
+    }
+});
+// We should write this for the route "/" at the end so if any of the function throws error it will handle them.
 
 
-
-app.use("/admin",
-    adminAuth)
-
-app.get("/admin/getAllData",
-(req,res)=>{
-  res.send("All Data Sent")  
-})
-
-app.delete("/admin/deleteUser",
-    (req,res)=>{
-      res.send("User Deleted")  
-    })
-
-
-
-
-
-
-app.listen(2000,()=>{
-    console.log("Server listening on 2000")
-})
-
-
-
-
-
+app.listen(2000, () => {
+    console.log("Server is running on port 2000");
+});
