4759b10cba2fde5c93f902a512b184c823401d00
divyanshisingh08 <divyanshisingh2308@gmail.com>
2024-12-27 15:07:28 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,35 +2,79 @@ const express=require("express")
 
 const app= express();
 
+//ROUTES
+
+// app.get("/user",(req,res)=>{
+//     res.send({firstName: "Divyanshi"} )
+// })
+// app.post("/user",(req,res)=>{
+//     res.send("Data Saved Successfullu")
+// })
+
+// app.delete("/user",(req,res)=>{
+//     res.send("User Deleted ")
+// })
+// app.get("/a(bc)?d",(req,res)=>{
+//     res.send("a(bc)?d" )
+// })
+
+// app.get("/ab*c",(req,res)=>{
+//     res.send("abbbbbbbbbbc" )
+// })
 
 
-app.get("/user",(req,res)=>{
-    res.send({firstName: "Divyanshi"} )
-})
-app.post("/user",(req,res)=>{
-    res.send("Data Saved Successfullu")
-})
+// app.get(/.*fly$/,(req,res)=>{
+//     res.send("Ends with fly" )
+// })
 
-app.delete("/user",(req,res)=>{
-    res.send("User Deleted ")
-})
-app.get("/a(bc)?d",(req,res)=>{
-    res.send("a(bc)?d" )
-})
-
-app.get("/ab*c",(req,res)=>{
-    res.send("abbbbbbbbbbc" )
-})
+// app.get("/user/:userId/:password",(req,res)=>{
+//     console.log(req.params)
+//     res.send("query params")
+// })
 
 
-app.get(/.*fly$/,(req,res)=>{
-    res.send("Ends with fly" )
-})
+//ROUTE HANDLERS & MIDDLEWARES
 
-app.get("/user/:userId/:password",(req,res)=>{
-    console.log(req.params)
-    res.send("query params")
-})
+app.use(
+    "/user",
+
+    (req,res,next)=>{
+        console.log("Handling Route handler 1")
+        // res.send("Response 1"),
+    next()
+    },
+
+    
+    (req,res,next)=>{
+        console.log("Handling Route handler 2")
+        // res.send("Response 2"),
+        next()
+    },
+
+    (req,res,next)=>{
+        console.log("Handling Route handler 3")
+        // res.send("Response 3"),
+        next()
+    },
+    
+    
+    (req,res,next)=>{
+        console.log("Handling Route handler 4")
+        next()
+      
+    }
+    
+
+   )
+
+//We can write this route handle as any of them
+/**
+ * app.use("/user",RouteHandler1,RouteHandler2,RouteHandler3,RouteHandler4)
+ * app.use("/user", [RouteHandler1,RouteHandler2,RouteHandler3,RouteHandler4])
+ * app.use("/user", [RouteHandler1],RouteHandler2,RouteHandler3,RouteHandler4)
+ * app.use("/user", [RouteHandler1,RouteHandler2],RouteHandler3,RouteHandler4)
+ * app.use("/user", RouteHandler1,[RouteHandler2,RouteHandler3,RouteHandler4])
+ */
 
 
 
