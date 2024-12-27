95bc8afc3788e88d8eacb3970ba49e5cb10a62d5
divyanshisingh08 <divyanshisingh2308@gmail.com>
2024-12-27 15:42:48 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use(
     },
 
     
-    (req,res,next)=>{
+ [ (req,res,next)=>{
         console.log("Handling Route handler 2")
         // res.send("Response 2"),
         next()
@@ -53,14 +53,13 @@ app.use(
 
     (req,res,next)=>{
         console.log("Handling Route handler 3")
-        // res.send("Response 3"),
+        res.send("Response 3"),
         next()
-    },
-    
+    }],
     
     (req,res,next)=>{
         console.log("Handling Route handler 4")
-        next()
+        // next()
       
     }
     
@@ -82,3 +81,18 @@ app.listen(2000,()=>{
     console.log("Server listening on 2000")
 })
 
+
+
+
+
+/**
+ * 
+ * GET /users: It will go through all the app.xxx("matching route") functions until it reaches the function that sends the response back. 
+        The function that sends the response is called the Request Handler.
+        All the functions it passes through along the way are called Middlewares.
+ * 
+ * 
+ *  Whenever you make an API call , it goes through middleware chain and finally it goes to Request Handler (The function which is sending response back)
+ * 
+ * The job of express js is - to take request and send response back
+ */
