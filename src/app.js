88d97b9d7f387e2ebe440126fff6a2839a637594
divyanshisingh08 <divyanshisingh2308@gmail.com>
2025-01-02 23:08:45 +0530

    delete API

--- src/app.js
+++ src/app.js
@@ -1,72 +1,67 @@
 const express = require("express");
 // require("./config/database")
-const connectDB=require("./config/database")
+const connectDB = require("./config/database");
 const app = express();
-const User=require("./models/user")
+const User = require("./models/user");
 
-//Middleware to read a JSON file. 
+//Middleware to read a JSON file.
 app.use(express.json());
-//API creation for signup 
+//API creation for signup
 
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
 
-app.post("/signup",async (req,res)=>{
-    
-   console.log(req.body)
+  //Creating new instance of user model
+  const user = new User(req.body);
 
-//Creating new instance of user model
-const user= new User(req.body);
+  //user.save will return a promise therefore we have to use await since it is an async function
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
+});
 
-//user.save will return a promise therefore we have to use await since it is an async function
-try
-{
-    await user.save ();
-    res.send("User added successfully")
-}
-catch(err){
-res.status(400).send("Error saving the user: " +err.message)
-}
-})
+app.get("/user", async (req, res) => {
+  //Suppose we have to find a user by an emailId
 
+  const userEmail = req.body.emailId;
 
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length == 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
-app.get("/user",async (req,res)=>{
+app.delete("/delete", async (req, res) => {
 
-    //Suppose we have to find a user by an emailId
-
-    const userEmail=req.body.emailId;
 
     try{
-        const user=await User.find({emailId:userEmail})
-        if(user.length==0){
-            res.status(404).send("User Not Found")
-        }
-        else{
-        res.send(user)
-        }
+        const UserId = await User.findByIdAndDelete({_id})
+        res.send("User Deleted Successfully")
     }
     catch(err){
-
         res.status(400).send("Something went wrong")
 
     }
-
-})
-
-
+});
 
 //Feed API- to get all the users from the database.
-app.get("/feed", async(req,res)=>{
-
-    try{
-
-        const users=await User.find({});
-        res.send(users)
-    }
-    catch(err){
-        res.status(400).send("Something went wrong")
-    }
-
-})
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 /**
  * app.post("/signup",async (req,res)=>{
@@ -93,15 +88,13 @@ catch(err){
 })
  */
 
-
-
 connectDB()
-.then(()=>{
-    console.log("Database Connection Established")
+  .then(() => {
+    console.log("Database Connection Established");
     app.listen(2000, () => {
-        console.log("Server is running on port 2000");
+      console.log("Server is running on port 2000");
     });
-})
-.catch(()=>{
-    console.error("Database Connection Failed")
-})
+  })
+  .catch(() => {
+    console.error("Database Connection Failed");
+  });
