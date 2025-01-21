4d3bcde02538366d6da1941cb40f33c6e6a912a8
divyanshisingh08 <divyanshisingh2308@gmail.com>
2025-01-21 11:40:17 +0530

    ref and populate

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -6,6 +6,7 @@ const connectionRequestSchema = new mongoose.Schema({
     fromUserId:{
         type: mongoose.Schema.Types.ObjectId,
         required: true,
+        ref:"User" //reference to the user Collection
 
     },
     toUserId:{
--- src/routes/user.js
+++ src/routes/user.js
@@ -12,7 +12,15 @@ userRouter.get("/user/request/recieved",userAuth,async(req,res)=>{
         const connectionRequests= await connectionRequestModel.find({
             toUserId: loggedInUser._id,
             status: "interested"
-        })
+        }).populate("fromUserId",["firstName", "lastName"])
+
+
+        /* Now we have to save the data and get the details of the user liek firstname to know who has sent the request.
+        There are two ways to do the same:
+        1. user.save() and loop over all userId to get the details of the user
+
+        2. using ref to establish connection request between both tables and use populate 
+        */
         req.json({
             message:" Here are users interested in you",
             data: connectionRequests
